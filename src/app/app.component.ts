189d777e9abb5a64f0293083afad80c1385bc78b
isaleh <isaleh@generation-ia.com>
2021-04-30 14:37:41 +0200

    Devoir fait.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Utilisateur } from './utilisateur';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'projetAngularIbrahim';
+export class AppComponent{
+
+  formulaire: FormGroup;
+  listeUtilisateurs: Array<Utilisateur> = [];
+  users: Utilisateur;
+
+  constructor(private fb: FormBuilder) {
+
+    this.formulaire = this.fb.group({
+
+      nom : [null,  [Validators.required, Validators.pattern('[A-Z][a-z]+')]],
+      prenom : [null, [Validators.required, Validators.pattern('[A-Z][a-z]+')]],
+      age : [null, [Validators.required, Validators.pattern('[0-9]+')]],
+      email :  [null, [Validators.email, Validators.required]]
+    })
+   }
+
+  get nom(){
+
+    return this.formulaire.get('nom');
+  }
+
+  get prenom(){
+
+    return this.formulaire.get('prenom');
+  }
+
+  get age(){
+
+    return this.formulaire.get('age');
+  }
+
+  get email(){
+
+    return this.formulaire.get('email');
+  }
+
+  submit() {
+
+    if(this.formulaire.valid) {
+
+      this.users = new Utilisateur(this.nom.value, this.prenom.value, this.age.value, this.email.value);
+      this.listeUtilisateurs.push(this.users);
+    }
+  }
 }
---
+++ src/app/utilisateur.ts
@@ -0,0 +1,15 @@
+export class Utilisateur {
+
+  nom: string;
+  prenom: string;
+  age: number;
+  email: string;
+
+  constructor(nom: string, prenom: string, age: number, email: string) {
+
+     this.nom = nom;
+     this.prenom = prenom;
+     this.age = age;
+     this.email = email;
+   }
+}
